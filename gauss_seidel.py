07362ae2f0f2888b003336bc85a50ee6063214d7
Alexander Jun <yunjae06.jun@gmail.com>
2026-09-06 20:24:58 -0500

    Began implementation of polynomial acceleration, still not functional.

--- gauss_seidel.py
+++ gauss_seidel.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy.linalg as sp
+import scipy.sparse.linalg as sl
+from scipy.sparse.linalg import LinearOperator
 import math
 
 #Iterative solver of Ax=b given matrix splitting A = M - N, initial x value of x0, and error bound e
@@ -79,55 +81,92 @@ def SSOR_sampler(A, mean=None, y0=None, w=1, k_max=300):
         y = sp.solve_triangular(np.transpose(M_SOR), np.transpose(N_SOR)@x + c*sqrtD@z, lower=False)
     return y+mean
 
-def poly_acc_gibbs_sampler(A, d_max, d_min, mean=None, y0=None, w=1, k_max=300):
+def cheby_acc_SSOR_sampler(A, d_max=None, d_min=None, w=1, mean=None, y0=None, k_max=300):
     n = np.shape(A)[0]
-    D = np.diag(np.diag(A))
-    sqrtD = np.sqrt(D)
-    M_SOR = (1/w)*D + np.tril(A, -1)
-    N_SOR = ((1-w)/w)*D - np.triu(A, 1)
-    alpha = 1
-    tau = 2/(d_max+d_min)
-    beta = tau
-    k = tau
-    c = math.sqrt((2/w - 1))
-    z = np.empty(n)
-    if mean is None:
-        mean = np.zeros(n)
+    d = np.diag(A)
+    D = (2/w-1)*d
+    M = (1/w)*np.diag(d) + np.tril(A, -1)
+    N = ((1-w)/w)*np.diag(d) - np.triu(A, 1)
     if y0 is None:
         y = np.zeros(n)
     else:
-        y = np.copy(y0)
+        y = y0
+    y_prev = np.empty(n)
+    if mean is None:
+        mean = np.zeros(n)
+    else:
+        mean = A@mean
+    def app_MA(v):
+        v1 = np.diag(d) @ sp.solve_triangular(M, A @ v)
+        return sp.solve_triangular(np.transpose(M), v1)
+    B = LinearOperator((n, n), app_MA)
+    if d_max is None:
+        max_vals, _ = sl.eigs(B, k=1, which='LM')
+        d_max = max_vals[0]
+        print(d_max)
+    if d_min is None:
+        min_vals, _ = sl.eigs(B, k=1, sigma=0, which='LM')
+        d_min = min_vals[0]
+        print(d_min)
+    delta = math.pow(((d_max-d_min)/4), 2)
+    tau = 2/(d_max+d_min)
 
+    beta = tau
+    alpha = 1
+    b = 2/alpha-1
+    a = (2/tau-1)*b
+    k = tau
+    z = np.empty(n)
     for i in range(k_max):
-        beta = 1/(1/tau - beta * ((d_max-d_min)/4)**2)
-        alpha = beta / tau
-        b = 2*(1-alpha)/alpha*(k/tau)+1
-        a = (2-tau)/tau + (b-1)(1/tau+1/k-1)
-        noise_var = a*M_SOR + b*N_SOR
+        for m in range(n):
+            z[m] = np.random.normal(0, b*D[m])
+        x = y + sp.solve_triangular(M, z - A@y, lower=True)
+        for m in range(n):
+            z[m] = np.random.normal(0, a*D[m])
+        vv = x - y + sp.solve_triangular(np.transpose(M), z - A@x, lower=False)
 
+        if i==0:
+            y_prev = y
+            y = alpha*(y + tau*vv)
+        else:
+            temp = np.copy(y)
+            y = y_prev + alpha*(y - y_prev + tau*vv)
+            y_prev = temp
+        beta = 1/(1/tau - beta*delta)
+        alpha = beta/tau
+        b = 2*k*(1-alpha)/beta + 1
+        a = (2/tau-1) + (b-1)*(1/tau+1/k-1)
+        k = beta + (1-alpha)*k
+    return y+mean
 
+def findMA(A, w=1):
+    D = np.diag(A)
+    M = (1/w)*np.diag(D) + np.tril(A, k=-1)
+    return sp.inv(M @ np.diag(1/D) @ np.transpose(M)) @ A
 
 b = np.array([1.0, -1.0, 0.0, 2.])
 
-A = np.array([[7., 3, 2, 5],
-               [3, 4, 1, 3],
-               [2, 1, 3, 2],
-               [5, 3, 2, 6]])
+A = np.array([[7., .3, .2, .5],
+               [.3, 4, .1, .3],
+               [.2, .1, 3, .2],
+               [.5, .3, .2, 6]])
 target_mean = np.array([3, -1, -2, 6])
 
-test_size = 300
+test_size = 200
 test_set = np.empty((test_size, 4))
 test_set2 = np.empty((test_size, 4))
-#for x in range(test_size):
-    #test_set[x] = gibbs_sampler(A, mean=target_mean)
-    #test_set2[x] = SSOR_sampler(A, mean=target_mean)
+for x in range(test_size):
+    test_set[x] = SSOR_sampler(A, mean=target_mean)
+    #test_set2[x] = cheby_acc_SSOR_sampler(A, mean=target_mean, d_min=0.251928, d_max=1)
+    test_set2[x] = cheby_acc_SSOR_sampler(A, mean=target_mean)
 
-#print(np.mean(test_set, axis=0))
-#cov = np.cov(np.transpose(test_set))
-#precision = sp.inv(cov)
-#print(precision)
-#print()
-#print(np.mean(test_set2, axis=0))
-#cov2 = np.cov(np.transpose(test_set2))
-#precision2 = sp.inv(cov2)
-#print(precision2)
+#print(findMA(A))
+print(np.mean(test_set, axis=0))
+cov = np.cov(np.transpose(test_set))
+precision = sp.inv(cov)
+print(precision)
+print()
+print(np.mean(test_set2, axis=0))
+cov2 = np.cov(np.transpose(test_set2))
+precision2 = sp.inv(cov2)
+print(precision2)
--- gauss_seidel_testing.py
+++ gauss_seidel_testing.py
@@ -1,23 +1,24 @@
 import numpy as np
 import scipy.linalg as sp
+import scipy.stats
 import matplotlib.pyplot as ppt
 import gauss_seidel as gs
 import math
 
-A = np.array([[5.1, 1.2], 
-              [1.2, 8.7]])
-b = np.array([20, -13])
+A = np.array([[13.2, 1.9], 
+              [1.9, 2.1]])
+b = np.array([-3, 4])
 c = sp.solve(A, b)
 delta = 0.025
-x = np.arange(-10, 15, delta)
-y = np.arange(-20, 10, delta)
+x = np.arange(-3, 1, delta)
+y = np.arange(-1, 6, delta)
 X, Y = np.meshgrid(x, y)
 xTAx = A[0][0]*X*X + A[1][0]*Y*X + A[0][1]*X*Y + A[1][1]*Y*Y
 Z = 0.5*xTAx - (b[0]*X + b[1]*Y)
 
 #Plots solution to Ax=b by plotting contours of x^T A x - b x
-
-GS = ppt.contour(X, Y, Z, levels=10)
+ppt.figure(0)
+GS = ppt.contour(X, Y, Z, levels=16)
 
 def plotted_gauss_seidel(A, b, x0=None, e=0.01):
     #A = L + D + U, M = L + D, N = -U
@@ -62,29 +63,15 @@ def plotted_gibbs_sampler(A, mean=None, y0=None, k_max=200, color = 'r'):
     return y
 
 #plotted_gauss_seidel(A, b)
-#for x in range(3):
-    #print(plotted_gibbs_sampler(A, c, k_max = 8))
+#print(plotted_gibbs_sampler(A, c, k_max = 8))
+#print(plotted_gibbs_sampler(A, c, k_max = 8, color = 'b'))
+#print(plotted_gibbs_sampler(A, c, k_max = 8, color = 'k'))
 
 #ppt.show()
 
-A = np.array([
-    [ 82,   1, -31,  -4,  25, -12,  -1,   3, -19,   7,   6,  15, -12,   1,   1],
-    [  1,  98, -33,  14, -26,  -2,  23,  15,  11,   5,  11,   5,   2, -20, -11],
-    [-31, -33,  69,   4,   1,   1, -14, -22,  10,  -3, -21,  -1,  15,   8,   6],
-    [ -4,  14,   4,  69,  25, -17,  29, -14,   2,  12,  12,   3, -22,  -4,  12],
-    [ 25, -26,   1,  25,  86, -29,  -5, -21, -25,  18,   5,   7, -19,  -1,   2],
-    [-12,  -2,   1, -17, -29,  46,  11,  11,   9,   0,  -4,  -4,  11,  -1,  -4],
-    [ -1,  23, -14,  29,  -5,  11,  87,  -7,   7,  17,  -5, -11,  -2,   5,  16],
-    [  3,  15, -22, -14, -21,  11,  -7,  54,  -3,  12,  11,   0, -16,  -3, -21],
-    [-19,  11,  10,   2, -25,   9,   7,  -3,  69,  17,   0,  -5,  20,   8,  13],
-    [  7,   5,  -3,  12,  18,   0,  17,  12,  17,  75,  -5,   2,  -4,   4,   9],
-    [  6,  11, -21,  12,   5,  -4,  -5,  11,   0,  -5,  82,  -4,  -5,   1,   1],
-    [ 15,   5,  -1,   3,   7,  -4, -11,   0,  -5,   2,  -4,  42,  -7,  -8,   3],
-    [-12,   2,  15, -22, -19,  11,  -2, -16,  20,  -4,  -5,  -7,  77,  -4,  11],
-    [  1, -20,   8,  -4,  -1,  -1,   5,  -3,   8,   4,   1,  -8,  -4,  36,  10],
-    [  1, -11,   6,  12,   2,  -4,  16, -21,  13,   9,   1,   3,  11,  10,  76]
-])
-target_mean = np.array([3, -1, -2, 6, 7, 4, -3, -5, 0, -1, 2, 1, 2, -3, 8])
+iw = scipy.stats.invwishart(df=220, scale=np.identity(200))
+A = iw.rvs()
+target_mean = np.zeros(200)
 
 def SSOR_gibbs_convergence(A, mean=None, y0=None, w=1, k_max=300, test_size = 300, print_cov = False):
     n = np.shape(A)[0]
@@ -119,7 +106,10 @@ def SSOR_gibbs_convergence(A, mean=None, y0=None, w=1, k_max=300, test_size = 30
         test_mean = np.mean(test_set, axis=0)
         mean_error = sp.norm(test_mean, 2)
         pre_error = sp.norm(test_cov - exp_cov, 2)
-        print(f"Iteration {k}: Mean error {mean_error}, Covariance error {pre_error}")
+        print(f"Iteration {k+1}: Mean error {mean_error}, Covariance error {pre_error}")
+        if(k>0):
+            ppt.plot([k, k+1], [last_error, pre_error], 'k')
+        last_error = pre_error
         if(print_cov):
             print(test_cov)
     return test_set+mean
@@ -162,4 +152,6 @@ def SSOR_gibbs_convergence_KL_div(A, mean=None, y0=None, w=1, k_max=300, test_si
             print(test_pre)
     return test_set+mean
 
-SSOR_gibbs_convergence(A, target_mean, k_max = 500, test_size=800)
+ppt.figure(1)
+SSOR_gibbs_convergence(A, target_mean, k_max = 300, test_size=600)
+ppt.show()
